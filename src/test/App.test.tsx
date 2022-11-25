57cc907687826f1592086af9c6141184d5f7e809
Joey <joeyko1004@gmail.com>
2022-11-25 12:42:17 +0800

    feat: add husky, eslint and vitest

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -3,5 +3,7 @@ import renderer from 'react-test-renderer'
 import App from '../App'
 
 test('simple render', () => {
-  renderer.create(<App />)
+  const component = renderer.create(<App />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 })
